551d3a31ee20bf6e87663694992f1d0e2e7a3177
Genbo Dev <genbodev@gmail.com>
2018-03-07 00:27:41 +0800

    Страница 'Сотрудничество' готова. Обновлены глобальные стили

--- src/pages/CooperationPage/CooperationPage.js
+++ src/pages/CooperationPage/CooperationPage.js
@@ -1,13 +1,98 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './CooperationPage.css';
 import { connect } from 'react-redux';
+import { StickyContainer, Sticky } from 'react-sticky';
+import TopPanelComponent from '../../components/TopPanelComponent/TopPanelComponent';
+import MainNavigation from '../../components/MainNavigation/MainNavigation';
+import MobileNavigation from '../../components/MobileNavigation/MobileNavigation';
+import Footer from '../../components/FooterComponent/FooterComponent';
+import { Grid, Row, Col } from 'react-bootstrap';
+import cooperationImage from './partnerka.jpg';
+
+function drawSticky(stickyComponent) {
+    return (
+        <Sticky>
+            {
+                ({
+                     style,
+
+                     // the following are also available but unused in this example
+                     isSticky,
+                     wasSticky,
+                     distanceFromTop,
+                     distanceFromBottom,
+                     calculatedHeight
+                 }) => {
+                    return (
+                        <div style={style} className={isSticky ? 'sticky' : 'sticky-default'}>
+                            {stickyComponent}
+                        </div>
+                    )
+                }
+            }
+        </Sticky>);
+}
 
 class CooperationPage extends Component {
     render() {
         return (
-            <div className="App">
-
+            <div id="CooperationPage">
+                <TopPanelComponent/>
+                <StickyContainer>
+                    {drawSticky(<MainNavigation/>)}
+                    <MobileNavigation/>
+                    <Fragment>
+                        <div className="cooperation-description">
+                            <Grid>
+                                <Row>
+                                    <Col xs={12}>
+                                        <h1>Сотрудничество</h1>
+                                        <p className="cooperation-description-paragraph-lead">
+                                            Компания POLIVAN GROUP - один из быстро растущих лидеров на рынке террасной
+                                            доски и изделий из ДПК в Владивостоке и на всем Дальнем востоке. Мы ищем
+                                            сотрудничество с ведущими продавцами систем благоустройства загородного
+                                            дома, Являясь инновационным производителем ДПК в России мы готовы
+                                            гарантировать высокий уровень сервисного обслуживания нашим клиентам.
+                                        </p>
+                                        <p>
+                                            Важным направлением развития для нашей компании стало установление надежных
+                                            и долгосрочных отношений с дилерами нашей продукции в Москве, а также во
+                                            всех регионах России, Беларуси и Казахстана. Нашим партнерам мы готовы
+                                            предоставить наиболее выгодные условия поставок, оптовые цены и всестороннюю
+                                            рекламно-информационную поддержку.
+                                        </p>
+                                        <p>
+                                            Продукция поставляется дилерам в широком ассортименте с возможностью
+                                            доставки до Склада силами транспортной компании или отдельными машинами по
+                                            специальным ценам!
+                                        </p>
+                                        <p>
+                                            Чтобы стать дилером компании POLIVAN GROUP и приобретать наши товары на
+                                            дилерских условиях - вам достаточно связаться любым удобным вам способом. Мы
+                                            с радостью обсудим взаимовыгодные формы сотрудничества. При положительном
+                                            решении о сотрудничестве дилеру высылается Дилерский Договор с Приложениями.
+                                        </p>
+                                        <h3>
+                                            Дилерство в компании POLIVAN GROUP - это выгодно!
+                                        </h3>
+                                        <hr/>
+                                    </Col>
+                                </Row>
+                            </Grid>
+                        </div>
+                        <div className="cooperation-image">
+                            <Grid>
+                                <Row>
+                                    <Col xs={12}>
+                                        <img src={cooperationImage} alt="cooperation-image" className="img-responsive"/>
+                                    </Col>
+                                </Row>
+                            </Grid>
+                        </div>
+                    </Fragment>
+                </StickyContainer>
+                <Footer/>
             </div>
         );
     }
@@ -15,15 +100,11 @@ class CooperationPage extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return {
-
-    }
+    return {}
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-
-    }
+    return {}
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CooperationPage);
