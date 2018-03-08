ad7277719c21e23101c5e003e38e6b77a120a0d8
Oleksandr Kondrachuk <alexandrkondrachuk@gmail.com>
2018-03-08 17:22:59 +0200

    add:Cooperation component

---
+++ src/pages/IndexPage/Cooperation/Cooperation.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Grid, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
+
+import './Cooperation.css';
+
+class Cooperation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textOne: 'Вы ищите себе надежного партнера по поставкам',
+            textTwo: 'Древесно-полимерного композита',
+            textThree: 'Древесно-полимерного композита',
+            buttonContacts: 'Контакты',
+            buttonAbout: 'подробнее о нас.',
+            buttonContactsHref: '/contacts',
+            buttonAboutHref: '/about'
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        const {textOne, textTwo, textThree, buttonContacts, buttonAbout, buttonAboutHref, buttonContactsHref} = this.state;
+        return (
+            <div id="Cooperation">
+                <Grid>
+                    <Col md={8}>
+                        <p>{textOne}</p>
+                        <p><strong>{textTwo}</strong></p>
+                        <p>{textThree}</p>
+                    </Col>
+                    <Col md={4}>
+                        <div className="cooperation-buttons">
+                            <Button className="contacts-btn"><Link
+                                to={buttonContactsHref}>{buttonContacts}</Link></Button>
+                            <Button className="about-btn" bsStyle="link">
+                                <Link to={buttonAboutHref}>{buttonAbout}</Link></Button>
+                        </div>
+                    </Col>
+                </Grid>
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Cooperation;
--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -6,6 +6,7 @@ import './IndexPage.css';
 
 import { drawSticky } from '../../functions';
 import MainCarousel from './MainCarousel/MainCarousel';
+import Cooperation from './Cooperation/Cooperation';
 import TopPanelComponent from '../../components/TopPanelComponent/TopPanelComponent';
 import MainNavigation from '../../components/MainNavigation/MainNavigation';
 import MobileNavigation from '../../components/MobileNavigation/MobileNavigation';
@@ -26,7 +27,8 @@ class IndexPage extends Component {
                             {drawSticky(<MainNavigation/>)}
                             <MobileNavigation/>
                             <Fragment>
-                                    <MainCarousel/>
+                                <MainCarousel/>
+                                <Cooperation/>
                             </Fragment>
                         </StickyContainer>
                     </div>
